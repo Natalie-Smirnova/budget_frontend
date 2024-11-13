36e0acbf7cd28c968f4c70bcfb9999da86ad7b5e
nsmirnova <natalie.smirnowa@gmail.com>
2024-11-13 09:18:13 +0100

    create `Login` component

---
+++ budget-frontend/src/components/Login/Login.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import s from './style.module.css';
+import type {FormProps} from 'antd';
+import {Button, Checkbox, Form, Input} from 'antd';
+
+type FieldType = {
+    username?: string;
+    password?: string;
+    remember?: string;
+};
+
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+    console.log('Success:', values);
+};
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+};
+
+export const Login: React.FC = () => (
+    <div className={s.container}>
+        <Form
+            name="basic"
+            labelCol={{span: 8}}
+            wrapperCol={{span: 16}}
+            style={{maxWidth: 600}}
+            initialValues={{remember: true}}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item<FieldType>
+                name="username"
+                label="E-mail"
+                rules={[
+                    {
+                        type: 'email',
+                        message: 'The input is not valid E-mail!',
+                    },
+                    {
+                        required: true,
+                        message: 'Please input your E-mail!',
+                    },
+                ]}
+            >
+                <Input/>
+            </Form.Item>
+
+            <Form.Item<FieldType>
+                label="Password"
+                name="password"
+                rules={[{required: true, message: 'Please input your password!'}]}
+            >
+                <Input.Password/>
+            </Form.Item>
+
+            <Form.Item<FieldType> name="remember" valuePropName="checked" label={null}>
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item label={null}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    </div>
+);
+
